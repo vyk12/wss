ace435311b5025fd371a16ce48c07d334c5cdc6b
Victor <victor.thuillier@gmail.com>
2015-04-30 15:25:16 +0200

    integrate socket.io

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var http = require('http');
 var nodeStatic = require('node-static');
+var mongoose = require('mongoose');
 
-var file = new nodeStatic.Server('./public');
+var fileServer = new nodeStatic.Server('./public');
 
-http.createServer(function(request, response) {
+var server = http.createServer(function(request, response) {
     request.addListener('end', function() {
-        file.serve(request, response, function(e, res) {
+        fileServer.serve(request, response, function(e, res) {
         	if (e && (e.status === 404)) {
-                file.serveFile('/404.html', 404, {}, request, response);
+                fileServer.serveFile('/404.html', 404, {}, request, response);
             }
         });
     }).resume();
-}).listen(8080);
+}).listen(80);
+
+var io = require('socket.io').listen(server);
